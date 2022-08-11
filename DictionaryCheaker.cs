18c75765e07bfaf377c206df00c8bccc5ae8214a
tyankazu <200441117@ccmailg.meijo-u.ac.jp>
2022-08-11 17:21:34 +0900

    awse

---
+++ DictionaryCheaker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+public class DicCheak
+{
+	static void Main()
+    {
+        // fnameに作った辞書の名前をいれて！！
+        string fname = "dic_bug.txt";
+
+
+        var dicList = new List<string>();
+        StreamReader sr = new StreamReader(fname, Encoding.GetEncoding("UTF-8"));
+
+        string text = sr.ReadToEnd();
+
+        dicList.Add(text.Substring(0, text.IndexOf(',')));
+
+        sr.Close();
+    }
+}
